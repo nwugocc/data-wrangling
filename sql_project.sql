086ebf5b6bd91134eb5efe91dfb8127e386b1e16
Chika <chika.nwugo@gmail.com>
2019-08-12 07:00:35 -0400

    Submission 1

---
+++ sql_project.sql
@@ -0,0 +1,161 @@
+/* Welcome to the SQL mini project. For this project, you will use
+Springboard' online SQL platform, which you can log into through the
+following link:
+
+https://sql.springboard.com/
+Username: student
+Password: learn_sql@springboard
+
+The data you need is in the "country_club" database. This database
+contains 3 tables:
+    i) the "Bookings" table,
+    ii) the "Facilities" table, and
+    iii) the "Members" table.
+
+Note that, if you need to, you can also download these tables locally.
+
+In the mini project, you'll be asked a series of questions. You can
+solve them using the platform, but for the final deliverable,
+paste the code for each solution into this script, and upload it
+to your GitHub.
+
+Before starting with the questions, feel free to take your time,
+exploring the data, and getting acquainted with the 3 tables. */
+
+/* Q1: Some of the facilities charge a fee to members, but some do not.
+Please list the names of the facilities that do. */
+
+SELECT name AS facility
+FROM `Facilities`
+WHERE membercost =0
+LIMIT 0 , 30;
+
+/* Q2: How many facilities do not charge a fee to members? */
+
+SELECT COUNT( name ) as 'Count of facilities with no member fees'
+FROM `Facilities`
+WHERE membercost =0;
+
+/* Q3: How can you produce a list of facilities that charge a fee to members,
+where the fee is less than 20% of the facility's monthly maintenance cost?
+Return the facid, facility name, member cost, and monthly maintenance of the
+facilities in question. */
+
+SELECT facid, name AS facility, membercost, monthlymaintenance
+FROM `Facilities`
+WHERE membercost < monthlymaintenance * 0.2
+LIMIT 0 , 30;
+
+/* Q4: How can you retrieve the details of facilities with ID 1 and 5?
+Write the query without using the OR operator. */
+
+SELECT *
+FROM `Facilities`
+WHERE facid =1
+  UNION 
+SELECT *
+FROM `Facilities`
+WHERE facid =5
+LIMIT 0 , 30;
+
+/* Q5: How can you produce a list of facilities, with each labelled as
+'cheap' or 'expensive', depending on if their monthly maintenance cost is
+more than $100? Return the name and monthly maintenance of the facilities
+in question. */
+
+SELECT name AS facility, monthlymaintenance,
+   CASE WHEN monthlymaintenance >100 THEN 'expensive'
+      ELSE 'cheap' END AS cost_class
+FROM `Facilities`
+LIMIT 0 , 30;
+
+/* Q6: You'd like to get the first and last name of the last member(s)
+who signed up. Do not use the LIMIT clause for your solution. */
+
+SELECT firstname, surname
+FROM `Members`
+WHERE memid = (
+             SELECT MAX( memid )
+             FROM `Members` 
+               ); 
+
+/* Q7: How can you produce a list of all members who have used a tennis court?
+Include in your output the name of the court, and the name of the member
+formatted as a single column. Ensure no duplicate data, and order by
+the member name. */
+
+SELECT  DISTINCT CONCAT( surname, ', ', firstname ) AS member, name AS 'tennis court'
+FROM `Facilities` c
+  JOIN (
+      SELECT surname, firstname, facid
+      FROM `Members` a
+        JOIN (
+           SELECT DISTINCT memid, facid
+           FROM `Bookings`
+           WHERE facid =0 OR facid =1
+             )b ON a.memid = b.memid
+       ) d ON c.facid = d.facid
+ORDER BY member;
+
+/* Q8: How can you produce a list of bookings on the day of 2012-09-14 which
+will cost the member (or guest) more than $30? Remember that guests have
+different costs to members (the listed costs are per half-hour 'slot'), and
+the guest user's ID is always 0. Include in your output the name of the
+facility, the name of the member formatted as a single column, and the cost.
+Order by descending cost, and do not use any subqueries. */
+
+SELECT CONCAT( surname, ', ', firstname ) AS Patron, name AS facility, /*bookid, starttime,*/ cost
+FROM `Members` c
+  JOIN (
+        SELECT membercost, guestcost, memid, name, slots, bookid, starttime,
+          CASE WHEN memid >0 THEN membercost * slots 
+             WHEN memid =0 THEN guestcost * slots
+                END AS cost
+        FROM `Bookings` a 
+          JOIN (
+                SELECT * 
+                FROM `Facilities`
+                ) b ON a.facid=b.facid
+        ) d ON c.memid=d.memid 
+WHERE cost > 30 AND LEFT(starttime, 10) = '2012-09-14'
+ORDER BY cost DESC;
+
+/* Q9: This time, produce the same result as in Q8, but using a subquery. */
+
+SELECT CONCAT( surname, ', ', firstname ) AS Patron, name AS facility, /*bookid, starttime,*/ cost
+FROM `Members` c
+  JOIN (
+      SELECT membercost, guestcost, memid, name, slots, bookid, starttime,
+        CASE WHEN memid >0 THEN membercost * slots 
+             WHEN memid =0 THEN guestcost * slots
+                END AS cost
+      FROM `Facilities` a 
+        JOIN (
+            SELECT slots, bookid, memid, facid, starttime 
+            FROM `Bookings`
+            WHERE LEFT(starttime, 10) = '2012-09-14'
+             ) b ON a.facid=b.facid
+       ) d ON c.memid=d.memid AND cost > 30
+ORDER BY cost DESC;
+
+/* Q10: 
+
+Produce a list of facilities with a total revenue less than 1000.
+The output of facility name and total revenue, sorted by revenue. Remember
+that there's a different cost for guests and members! */
+
+SELECT name as facility, SUM(revenue) AS 'total revenue'
+FROM (
+    SELECT name, 
+        CASE WHEN memid >0 THEN membercost * slots 
+             WHEN memid =0 THEN guestcost * slots
+                END AS revenue
+      FROM `Facilities` a 
+        JOIN (
+            SELECT slots, memid, facid 
+            FROM `Bookings`
+             ) b ON a.facid=b.facid
+     ) c
+GROUP BY facility
+HAVING SUM(revenue) < 1000
+ORDER BY SUM(revenue) DESC;
